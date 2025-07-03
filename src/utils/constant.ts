100568447ad7151cb542b0f7f4ca8a9792c8c893
Raman Sharma <kukreti.rs@gmail.com>
2025-07-03 11:57:46 +0530

    Added more technology options.

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -15,15 +15,18 @@ export const BackendTechnologiesItems: IReactSelectOptions[] = [
   },
   { label: "ASP.NET MVC", value: "ASP.NET MVC", isDisabled: false },
   { label: "ASP.NET", value: "ASP.NET", isDisabled: false },
+  { label: "REST APIs", value: "REST APIs", isDisabled: false },
+  { label: "GIT", value: "GIT", isDisabled: false },
 ];
 
 export const FrontechnologiesItems: IReactSelectOptions[] = [
   { label: "Please Select", value: "-", isDisabled: true },
-  { label: "HTML", value: "HTML", isDisabled: false },
-  { label: "CSS", value: "CSS", isDisabled: false },
+  { label: "HTML5", value: "HTML5", isDisabled: false },
+  { label: "CSS3", value: "CSS3", isDisabled: false },
   { label: "JAVASCRIPT", value: "JAVASCRIPT", isDisabled: false },
   { label: "REACT", value: "REACT", isDisabled: false },
   { label: "Angular 2+", value: "Angular 2+", isDisabled: false },
   { label: "Bootstrap", value: "Bootstrap", isDisabled: false },
   { label: "Tailwind CSS", value: "Tailwind CSS", isDisabled: false },
+  { label: "Responsive Design", value: "Responsive Design", isDisabled: false },
 ];
